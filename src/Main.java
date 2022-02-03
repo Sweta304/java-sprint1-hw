b6f55072fd68ad12c975286ec33b8d0110ea1416
Lana <box4svv@yandex.ru>
2022-02-03 22:53:49 +0300

    - added try-catch block for command input - changed printMenu() modifier to private

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,12 +9,12 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        int year=0;
+        int year = 0;
 
-        HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports=new HashMap<>();
-        ArrayList<HashMap<Integer,Integer>> yearReport = new ArrayList<>();
-        boolean isMonthReportsCalled=false;
-        boolean isYearReportCalled=false;
+        HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports = new HashMap<>();
+        ArrayList<HashMap<Integer, Integer>> yearReport = new ArrayList<>();
+        boolean isMonthReportsCalled = false;
+        boolean isYearReportCalled = false;
 
         YearlyReportReader yearlyReportReader = new YearlyReportReader();
         MonthlyReportReader monthlyReportReader = new MonthlyReportReader();
@@ -21,49 +22,56 @@ public class Main {
         YearlyReportOperations yearlyReportOperations = new YearlyReportOperations();
 
 
-        while(true){
+        while (true) {
             printMenu();
-            int command = scanner.nextInt();
-        if(command == 1) {
-            monthlyReports = monthlyReportReader.readMonthlyReports();
-            isMonthReportsCalled=true;
-        }else if(command == 2) {
-            System.out.println("введите год для считывания отчета");
-            year = scanner.nextInt();
-            yearReport = yearlyReportReader.readYearReports(year);
-            isYearReportCalled=true;
-        }else if(command == 3) {
-            if (isMonthReportsCalled & isYearReportCalled) {
-                ReportsMatching reportsMatching = new ReportsMatching(year, monthlyReports, yearReport);
-                reportsMatching.reportsMatching(yearlyReportOperations.monthQty(yearReport));
-            } else {
-                System.out.println("Сначала считайте все месячные и годовой отчеты");
+            int command;
+            try {
+                command = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Введите числовое значение");
                 break;
             }
-        }else if(command == 4){
-            if(isMonthReportsCalled){
-                monthlyReportsOperations.printMonthReport(monthlyReports);
-            } else {
-                System.out.println("Сначала считайте все месячные отчеты");
+            if (command == 1) {
+                monthlyReports = monthlyReportReader.readMonthlyReports();
+                isMonthReportsCalled = true;
+            } else if (command == 2) {
+                System.out.println("введите год для считывания отчета");
+                year = scanner.nextInt();
+                yearReport = yearlyReportReader.readYearReports(year);
+                isYearReportCalled = true;
+            } else if (command == 3) {
+                if (isMonthReportsCalled & isYearReportCalled) {
+                    ReportsMatching reportsMatching = new ReportsMatching(year, monthlyReports, yearReport);
+                    reportsMatching.reportsMatching(yearlyReportOperations.monthQty(yearReport));
+                } else {
+                    System.out.println("Сначала считайте все месячные и годовой отчеты");
+                    break;
+                }
+            } else if (command == 4) {
+                if (isMonthReportsCalled) {
+                    monthlyReportsOperations.printMonthReport(monthlyReports);
+                } else {
+                    System.out.println("Сначала считайте все месячные отчеты");
+                    break;
+                }
+            } else if (command == 5) {
+                if (isYearReportCalled) {
+                    yearlyReportOperations.printYearReport(year, yearReport);
+                } else {
+                    System.out.println("Сначала считайте годовой отчет");
+                    break;
+                }
+            } else if (command == 0) {
+                System.out.println("Выход");
                 break;
-            }
-        }else if(command == 5){
-            if(isYearReportCalled){
-                yearlyReportOperations.printYearReport(year,yearReport);
             } else {
-                System.out.println("Сначала считайте годовой отчет");
-                break;
+                System.out.println("Извините, такой команды пока нет.");
             }
-        } else if (command == 0) {
-            System.out.println("Выход");
-            break;
-        } else {
-            System.out.println("Извините, такой команды пока нет.");
-        }
         }
 
     }
-    public static void printMenu() {
+
+    private static void printMenu() {
         System.out.println("Выберите команду:");
         System.out.println("1 - Считать все месячные отчёты\n" +
                 "2 - Считать годовой отчёт\n" +
