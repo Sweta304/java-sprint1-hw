b63f680efaf47099fb24e08fbcf32b9e9aa7f2cd
Lana <box4svv@yandex.ru>
2022-02-05 16:22:22 +0300

    added "commonCashFlow" method

--- src/MonthlyReportsOperations.java
+++ src/MonthlyReportsOperations.java
@@ -5,7 +5,7 @@ public class MonthlyReportsOperations {
 
     private ConvertMonthNumsIntoNames month = new ConvertMonthNumsIntoNames();
 
-    public int commonExpensesByMonth(int monthNumber, HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports) {
+    private int commonCashFlow(String isExpense, int monthNumber, HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports){
         int commonExpenses = 0;
         HashMap<String, ArrayList<String>> monthReport = monthlyReports.get(monthNumber);
         ArrayList<String> itemsValues;
@@ -19,21 +19,18 @@ public class MonthlyReportsOperations {
         return commonExpenses;
     }
 
-    public int commonIncomesByMonth(int monthNumber, HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports) {
-        int commonIncomes = 0;
-        HashMap<String, ArrayList<String>> monthReport = monthlyReports.get(monthNumber);
-        ArrayList<String> itemsValues;
+    public int commonExpensesByMonth(int monthNumber, HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports) {
+        int commonExpenses = commonCashFlow("TRUE",monthNumber,monthlyReports);
+        return commonExpenses;
+    }
 
-        for (String i : monthReport.keySet()) {
-            itemsValues = monthReport.get(i);
-            if (itemsValues.get(0).equals("FALSE")) {
-                commonIncomes = commonIncomes + (Integer.valueOf(itemsValues.get(1)) * Integer.valueOf(itemsValues.get(2)));
-            }
-        }
+    public int commonIncomesByMonth(int monthNumber, HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports) {
+        int commonIncomes = commonCashFlow("FALSE",monthNumber,monthlyReports);
         return commonIncomes;
     }
 
-    public void theMostProfitableItem(int monthNumber, HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports) {
+    private String findItem(String isExpense, int monthNumber, HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports) {
+        String foundItem;
         int maxProfit = 0;
         String theMostProfitableItem = null;
         HashMap<String, ArrayList<String>> monthReport = monthlyReports.get(monthNumber);
@@ -41,7 +38,7 @@ public class MonthlyReportsOperations {
 
         for (String i : monthReport.keySet()) {
             itemsValues = monthReport.get(i);
-            if (itemsValues.get(0).equals("FALSE")) {
+            if (itemsValues.get(0).equals(isExpense)) {
                 int itemProfit = Integer.valueOf(itemsValues.get(1)) * Integer.valueOf(itemsValues.get(2));
                 if (itemProfit > maxProfit) {
                     maxProfit = itemProfit;
@@ -49,26 +46,18 @@ public class MonthlyReportsOperations {
                 }
             }
         }
-        System.out.println("самый прибыльный товар: " + theMostProfitableItem + " - " + maxProfit);
+        foundItem = theMostProfitableItem + " - " + maxProfit;
+        return foundItem;
+    }
+
+    public void theMostProfitableItem(int monthNumber, HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports) {
+        String foundItem = findItem("FALSE",monthNumber,monthlyReports);
+        System.out.println("самый прибыльный товар: " + foundItem);
     }
 
     public void theMostExpenseItem(int monthNumber, HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports) {
-        int maxExpense = 0;
-        String theMostExpenseItem = null;
-        HashMap<String, ArrayList<String>> monthReport = monthlyReports.get(monthNumber);
-        ArrayList<String> itemsValues;
-
-        for (String i : monthReport.keySet()) {
-            itemsValues = monthReport.get(i);
-            if (itemsValues.get(0).equals("TRUE")) {
-                int itemProfit = Integer.valueOf(itemsValues.get(1)) * Integer.valueOf(itemsValues.get(2));
-                if (itemProfit > maxExpense) {
-                    maxExpense = itemProfit;
-                    theMostExpenseItem = i;
-                }
-            }
-        }
-        System.out.println("самая большая трата: " + theMostExpenseItem + " - " + maxExpense);
+        String foundItem = findItem("TRUE",monthNumber,monthlyReports);
+        System.out.println("самая большая трата: " + foundItem);
     }
 
     public void printMonthReport(HashMap<Integer, HashMap<String, ArrayList<String>>> monthlyReports) {
